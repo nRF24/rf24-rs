86a0d95a27e88618f459de16a67620eb8a848358
Brendan <2bndy5@gmail.com>
2025-03-07 00:24:49 -0800

    push WIP toward embedded-rust examples
    use hardware trait

--- examples/rust/src/lib.rs
+++ examples/rust/src/lib.rs
@@ -1,8 +1,23 @@
 //! A helper library meant to abstract platform-specific implementation details
 //! from examples that are supposed to be platform-agnostic.
 #![no_std]
-#[cfg(feature = "linux")]
+
+pub mod hal_impl_trait;
+
+#[cfg(target_os = "linux")]
 pub mod linux;
+#[cfg(target_os = "linux")]
+pub use linux as hal;
+
+#[cfg(all(feature = "rp2040"))]
+pub mod rp2040;
+#[cfg(all(feature = "rp2040"))]
+pub use rp2040 as hal;
+
+#[cfg(all(not(target_os = "none"), not(target_os = "linux"), not(feature = "rp2040")))]
+pub mod stubs;
+#[cfg(all(not(target_os = "none"), not(target_os = "linux"), not(feature = "rp2040")))]
+pub use stubs as hal;
 
 use anyhow::{anyhow, Error};
 use core::fmt::Debug;
